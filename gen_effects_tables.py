f5ccbf2852e6d2ba4a395b0aa259641ccb00c57f
Chris Angelico <rosuav@gmail.com>
2017-11-22 21:10:05 +1100

    Shorten comment

--- gen_effects_tables.py
+++ gen_effects_tables.py
@@ -51,8 +51,7 @@ effects = {
 		"TFCond_Jarated": "%s just got covered in Jarate. Eww.",
 		"TFCond_Milked": "%s just got covered in something that's almost, but not entirely, unlike milk.",
 		"TFCond_MarkedForDeathSilent": "%s needs to die. Go! Arrange that for me!",
-		# Will probably result in death. Thirty seconds unable to move is gonna suck.
-		"TFCond_HalloweenKartCage": "%s has been naughty and is now imprisoned.",
+		"TFCond_HalloweenKartCage": "%s has been naughty and is now imprisoned.", # Likely a death sentence
 		"TFCond_Plague": "A rat bites %s and inflicts a non-contagious form of the Bubonic Plague.",
 		# TFCond_RestrictToMelee, //TODO: If this gets triggered, also force selection of melee weapon
 		"view_as<TFCond>(-2)": "%s becomes as heavy as... well, a Heavy?",
