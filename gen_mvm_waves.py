31170cbbe2c38f6747778f808a1640ed63d61a33
Chris Angelico <rosuav@gmail.com>
2017-12-28 04:22:48 +1100

    Recreate my original pop file using the script

--- gen_mvm_waves.py
+++ gen_mvm_waves.py
@@ -12,6 +12,109 @@ population
 	RespawnWaveTime		6
 	CanBotsAttackWhileInSpawnRoom	no"""
 
+def make_wave(tanks=1, support=()):
+	info = """	Wave
+	{
+		WaitWhenDone	65
+		Checkpoint	Yes
+		StartWaveOutput
+		{
+			Target	wave_start_relay
+			Action	Trigger
+		}
+		DoneOutput
+		{
+			Target	wave_finished_relay
+			Action	Trigger
+		}
+"""
+	for i in range(tanks):
+		# Add the harbinger. The first one is a little bit different.
+		info += """		WaveSpawn
+		{
+			Name	"Harbinger %d"
+			%s
+			TotalCurrency	100
+			TotalCount	1
+			MaxActive	5
+			SpawnCount	2
+			Where	spawnbot
+			WaitBeforeStarting	%d
+			WaitBetweenSpawns	10
+			Squad
+			{
+				TFBot
+				{
+					Health	500
+					Name	Soldier
+					Class	Soldier
+					Skill	Normal
+					Item	"tf_weapon_rocketlauncher"
+					Item	"tf_weapon_shotgun_soldier"
+					Item	"tf_weapon_shovel"
+				}
+			}
+		}
+""" % (i + 1, 'WaitForAllDead	"Harbinger %d"' % i if i else '', 30 if i else 0)
+		# And add the tank itself.
+		info += """		WaveSpawn
+		{
+			Name	"Tank %d"
+			WaitForAllDead	"Harbinger %d"
+			TotalCurrency	400
+			TotalCount	1
+			MaxActive	5
+			SpawnCount	2
+			Where	spawnbot
+			WaitBeforeStarting	0
+			WaitBetweenSpawns	30
+			Squad
+			{
+				Tank
+				{
+					Health	40000
+					Name	Tank
+					Speed	75
+					StartingPathTrackNode	boss_path_1
+					OnKilledOutput
+					{
+						Target	boss_dead_relay
+						Action	Trigger
+					}
+					OnBombDroppedOutput
+					{
+						Target	boss_deploy_relay
+						Action	Trigger
+					}
+				}
+			}
+		}
+""" % (i+1, i+1)
+	for botclass in support:
+		info += """		WaveSpawn
+		{
+			TotalCurrency	400
+			TotalCount	10
+			MaxActive	5
+			SpawnCount	2
+			Where	spawnbot
+			WaitBeforeStarting	0
+			WaitBetweenSpawns	10
+			Support	1
+			Squad
+			{
+				TFBot
+				{
+					Template	%s
+				}
+			}
+		}
+""" % botclass
+	return info + "	}"
+
 with open("mvm_coaltown.pop", "w") as pop:
 	print(PREAMBLE % 1504, file=pop)
+	print(make_wave(tanks=5, support=["T_TFBot_Sniper"]), file=pop)
+	# Other useful support keywords: T_TFBot_Scout_Fish, T_TFBot_Demoman_Knight,
+	# T_TFBot_Pyro, T_TFBot_Demoman
 	print("}", file=pop)
