52748f442926b924750f39570627c30d084a3a89
Chris Angelico <rosuav@gmail.com>
2018-05-03 22:18:10 +1000

    mvm: Nerf Mannworks a little

--- gen_mvm_waves.py
+++ gen_mvm_waves.py
@@ -137,7 +137,7 @@ def subwave(botclass, count, *, max_active=5, spawn_count=2, money=None, chain=F
 	})
 	wave.money += pop.money(money * count)
 
-def harby_tanks(count, harby_money=None, tank_money=None, delay=30):
+def harby_tanks(count, *, harby_money=None, tank_money=None, tank_health=None, tank_speed=None, delay=30):
 	# NOTE: Calling this function twice within a wave will result in
 	# parallel streams of harbies and tanks. This can be extremely
 	# confusing and should usually be avoided.
@@ -175,9 +175,9 @@ def harby_tanks(count, harby_money=None, tank_money=None, delay=30):
 			"Where": "spawnbot",
 			"WaitBeforeStarting": 0,
 			"Squad": {"Tank": {
-				"Health": pop.tank_health,
+				"Health": tank_health or pop.tank_health,
 				"Name": "Tank",
-				"Speed": pop.tank_speed,
+				"Speed": tank_speed or pop.tank_speed,
 				"StartingPathTrackNode": next(pop.tank_path),
 				"OnKilledOutput": {
 					"Target": "boss_dead_relay",
@@ -398,7 +398,7 @@ with PopFile("mvm_mannworks.pop", starting_money=5002, harby_money=0, tank_healt
 		subwave("T_TFBot_Heavy", 5, max_active=1, spawn_count=1)
 	with wave:
 		harby_tanks(1)
-		subwave("T_TFBot_Pyro_Flaregun", 20)
+		subwave("T_TFBot_Sniper_Huntsman", 15)
 		subwave("T_TFBot_Demoman_Boom", 25)
 		support("T_TFBot_Pyro")
 	with wave:
@@ -418,7 +418,7 @@ with PopFile("mvm_mannworks.pop", starting_money=5002, harby_money=0, tank_healt
 		support("T_TFBot_Scout_Fish")
 	with wave: # Boss fight!
 		# Yes, that's right. Eight tanks... but none but harbies to carry the bomb.
-		for _ in range(2): harby_tanks(4, delay=100)
+		for _ in range(2): harby_tanks(4, delay=100, tank_health=20000)
 		# Bonus: a bit of Air Strike fodder to start things off. Helps if the
 		# wave has to be restarted.
 		subwave("Milkman", 10, max_active=10, spawn_count=10)
